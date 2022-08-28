87b43de3c8342392578e13ff8537935032ed8af3
Michael <mtse431@gmail.com>
2022-08-28 08:37:05 +0700

    Create Function to Activate Reset Button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const listOfTipButton = document.querySelectorAll(".btn-tip");
+let previousSelectedBtn;
+let tipAmount;
+const billAmount = document.getElementById("in-bill");
 const amountOfPeople = document.getElementById("in-amountOfPeople");
 
 amountOfPeople.addEventListener("change", function () {
@@ -6,4 +10,14 @@ amountOfPeople.addEventListener("change", function () {
     } else {
         amountOfPeople.parentElement.classList.remove("error");
     }
+    isResetAvail();
 })
+
+function isResetAvail() {
+    if (billAmount.value === "" || amountOfPeople.value === "" || tipAmount ==="") {
+        resetButton.disabled = true;
+    } else {
+        resetButton.disabled = false
+    }
+};
+
