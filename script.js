cce9118c84b6b37d4caa31d5fc05127625d4899e
Michael <mtse431@gmail.com>
2022-08-28 08:38:44 +0700

    Add Event Listener to Inputs

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ let previousSelectedBtn;
 let tipAmount;
 const billAmount = document.getElementById("in-bill");
 const amountOfPeople = document.getElementById("in-amountOfPeople");
+const resetButton = document.getElementById("reset-btn");
+
+billAmount.addEventListener("change", function () {
+    isResetAvail();
+})
+
+listOfTipButton.forEach(button => {
+    button.addEventListener("click", function (event) {
+        if (previousSelectedBtn) {
+            previousSelectedBtn.classList.remove("selected");
+            tipAmount = "";
+        }
+        previousSelectedBtn = event.target;
+        if (previousSelectedBtn.nodeName === "INPUT") {
+            if (previousSelectedBtn.value !== "") {
+                tipAmount = Number(previousSelectedBtn.value);
+            } else {
+                previousSelectedBtn.addEventListener("change", function () {
+                    tipAmount = Number(previousSelectedBtn.value);
+                })
+            }
+        } else {
+            previousSelectedBtn.classList.add("selected");
+            tipAmount = Number(previousSelectedBtn.id);
+        }
+        isResetAvail();
+    });
+});
 
 amountOfPeople.addEventListener("change", function () {
     if (Number(amountOfPeople.value) === 0) {
